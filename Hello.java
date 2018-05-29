bcd0211e6f8798dbed29dc14757e60efc675f151
Umamurugesan77 <umadurka1998@gmail.com>
2018-05-29 01:52:56 -0700

    byte

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello
+{
+public static void main(String args[])
+{
+byte one = 4;
+byte two = 6;
+int three = (int)one + (int)two;
+System.out.println("HelloWorld");
+System.out.println(three);
+}
+}
